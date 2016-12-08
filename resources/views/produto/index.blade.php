73ec869332d22196e0fb13e1c89d810fba56a804
karinealves <karine.jam@gmail.com>
2016-12-07 23:29:42 -0300

    mudancas ate agora

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -2,76 +2,94 @@
 
 @section('content')
 <div class="container">
-<h2>Produto</h2>
-<br><br>
-     <form method="POST" action="{{url('produto')}}" style="display:inline"> 
-      {{ csrf_field() }}        
-        <div class="form-group" >
-                <strong>Buscar produto:</strong>
-                <input class="form-control" placeholder="Produto" name="pesq" type="text" >
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1">
+      <div class="panel panel-success">
+        <div class="panel-heading">Produto</div>
+        <!--Um novo painel-->
+          <div class="panel-body">
+            <form class="form-horizontal" method="POST" action="{{url('produto')}}" style="display:inline"> 
+              {{ csrf_field() }}  
+              <div class="form-group">
+                  <div class="col-md-6">
+                        <div class="col-lg-10">
+                        <div class="input-group">
+                        <input class="form-control" placeholder="Buscar Produto..." name="pesq" type="text" >
+                      <span class="input-group-btn">
+                            <button class="btn btn-primary" type="submit">Buscar</button>
+                          </span>
+                </div>
+                </div>  
+               </div>
+           </div>
+
+             </form>
+    
+        <br>
+
+        <a class="btn btn-primary" href="{{url('/produto/create')}}" ><i class="glyphicon glyphicon-plus" aria-hidden="true"></i> Adicionar Produto </a>
+
+            @if ($message = Session::get('erro'))
+                <div class="alertr">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+             @if ($message = Session::get('success'))
+                <div class="alert">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+
+        <!-- Inicio da Tabela que mostra os resultados de Busca -->
+                    <div class="table-responsive">
+                      <table border = "1" class="table table-condensed">   
+                        <thead>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Valor R$</th>
+                            <th>Ações</th>
+                        </thead>
+                    <tbody>
+                    @foreach($produtos as $produto)
+                        <tr>
+                            <td>{{$produto->nome}} </td>
+                            <td>{{$produto->descricao}} </td>
+                            <td>R$ {{$produto->custo}} </td>
+                            <td>            
+                            
+                            <!--Botoes-->  
+                            <form method = "POST"  action="{{url('/produto/show')}}"  style="display:inline">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="produto" value="{{$produto->id}}">
+                                <input class="btn btn-mini" class="confirm" type="submit" value="Detalhes" />
+                            </form>   
+                            <form method = "POST"  action="{{url('/produto/edit')}}"  style="display:inline">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="produto" value="{{$produto->id}}">
+                                <input class="btn btn-mini" class="confirm" type="submit" value="Alterar"/>
+                            </form>   
+                            <form method = "POST"  action="{{url('/produto/delete')}}"  style="display:inline">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="produto" value="{{$produto->id}}">
+                                <input class="btn btn-mini" class="confirm" type="submit" value="Deletar" onclick="clicked(event)"/>
+                            </form>                 
+                            
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+               </div>
+    
         </div>
-        <div class="form-group" >
-                <button type="submit" class="btn" >Buscar</button>
-        </div>  
-     </form>
-
-<br>
-
-<a class="btn" href="{{url('/produto/create')}}"> Add Produto </a>
-
-    @if ($message = Session::get('erro'))
-        <div class="alertr">
-            <p>{{ $message }}</p>
         </div>
-    @endif
-     @if ($message = Session::get('success'))
-        <div class="alert">
-            <p>{{ $message }}</p>
         </div>
-    @endif
+        </div> 
+ 
+        @endsection
+        <script>
+            function clicked(e) {
+                if(!confirm('Deseja deletar este produto?'))e.preventDefault();
+            }
+        </script>
 
-<table class="table">
-    <thead>
-        <th>Nome</th>
-        <th>Descrição</th>
-        <th>Valor</th>
-        <th>Ações</th>
-
-    </thead>
-    <tbody>
-    @foreach($produtos as $produto)
-        <tr>
-            <td>{{$produto->nome}} </td>
-            <td>{{$produto->descricao}} </td>
-            <td>R$ {{$produto->custo}} </td>
-            <td>            
-            <div>
-            <!--Botoes-->  
-            <form method = "POST"  action="{{url('/produto/show')}}"  style="display:inline">
-                {{ csrf_field() }}
-                <input type="hidden" name="produto" value="{{$produto->id}}">
-                <input class="confirm" type="submit" value="Detalhes" "/>
-            </form>   
-            <form method = "POST"  action="{{url('/produto/edit')}}"  style="display:inline">
-                {{ csrf_field() }}
-                <input type="hidden" name="produto" value="{{$produto->id}}">
-                <input class="confirm" type="submit" value="Alterar"/>
-            </form>   
-            <form method = "POST"  action="{{url('/produto/delete')}}"  style="display:inline">
-                {{ csrf_field() }}
-                <input type="hidden" name="produto" value="{{$produto->id}}">
-                <input class="confirm" type="submit" value="Deletar" onclick="clicked(event)"/>
-            </form>                 
-            </div> 
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-</div>
-@endsection
-<script>
-    function clicked(e) {
-        if(!confirm('Deseja deletar este produto?'))e.preventDefault();
-    }
-</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+        <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
+        <div class="panel panel-defult">
+             <img class="img-responsive" src="banner.png">
+        </div>
+                
         </div>
     </div>
 </div>
